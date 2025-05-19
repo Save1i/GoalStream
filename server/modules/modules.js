99ee0db433a402ff1115233cd3fd17e56cf7f1ba
Save1i <savva0512@gmail.com>
2025-05-19 18:34:49 +0300

    add: img for user

--- server/modules/modules.js
+++ server/modules/modules.js
@@ -4,6 +4,7 @@ const sequelize = require("../db")
 const User = sequelize.define("user", {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
   name: { type: DataTypes.STRING, allowNull: false },
+  img: { type: DataTypes.STRING,  defaultValue: "null"},
   email: { type: DataTypes.STRING, unique: true },
   password: { type: DataTypes.STRING },
   role: { type: DataTypes.STRING, defaultValue: "USER" },
